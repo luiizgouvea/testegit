c6beef710a70cc55274ce42283629d3e0de957b8
Luiz <luiizgouvea@hotmail.com>
2020-05-30 20:53:11 -0300

    adição entrada idade

--- Entrada.java
+++ Entrada.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 public class Entrada {
     public static void main (String args[]){
         String nome;
+        int idade;
         
         Scanner tec = new Scanner (System.in);
 
         System.out.println("Qual seu nome?");
         nome=tec.nextLine();
+        System.out.println("Qual sua idade?");
+        idade=tec.nextInt();
 
     }
     
