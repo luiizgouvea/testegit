26fc4b212a4a8151aafe2b35b53e3fb9418d3b32
Luiz <luiizgouvea@hotmail.com>
2020-05-30 20:39:46 -0300

    nova mensagem adicionada

--- TestGit.java
+++ TestGit.java
@@ -1,5 +1,6 @@
 class TestGit{
     public static void main (String args[]){
         System.out.println("Olá, vamos iniciar o Git.");
+        System.out.println("Nova mensagem adicionada ao Aruivo.");
     }
 }
