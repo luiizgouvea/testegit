c03306fa4f7bffeacd56b2fe5d00704ff1a43b23
Luiz <luiizgouvea@hotmail.com>
2020-05-30 20:50:23 -0300

    adição função entrada

---
+++ Entrada.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Entrada {
+    public static void main (String args[]){
+        String nome;
+        
+        Scanner tec = new Scanner (System.in);
+
+        System.out.println("Qual seu nome?");
+        nome=tec.nextLine();
+
+    }
+    
+}
